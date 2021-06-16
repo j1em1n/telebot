796081764fed0f2fcf6384b59ad453c15a941ff4
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2021-06-16 23:49:05 +0800

    Change to blocking scheduler

--- asyncBot.py
+++ asyncBot.py
@@ -3,7 +3,7 @@ import os
 import random
 import sys
 import telebot
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 TOKEN = '1119831121:AAHb_nvYn1M5NciLJu1NX-48jMTkrPUZ0sc'
 CHAT_ID = 'https://t.me/hello_min'
@@ -24,6 +24,7 @@ if mode == "dev":
         updater.start_polling()
 elif mode == "prod":
     def run(updater):
+        HEROKU_APP_NAME = 'https://ou7is.herokuapp.com/'
         PORT = int(os.environ.get("PORT", "8443"))
         HEROKU_APP_NAME = os.environ.get(HEROKU_APP_NAME)
         # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
@@ -52,7 +53,7 @@ def send_message(bot):
     bot = telegram.Bot(token=TOKEN)
     bot.sendMessage(chat_id = CHAT_ID, text = 'Daily reminder has been set! You\'ll get notified at 11 AM daily')
 
-sched = BackgroundScheduler()
+sched = BlockingScheduler()
 
 # Runs from Monday to Friday at 11:30 (am) until 2014-07-30 00:00:00
 sched.add_job(send_message, 'cron', day_of_week='mon-fri', hour=11, minute=30, end_date='2021-07-30')
@@ -68,7 +69,7 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(CommandHandler("random", random_handler))
 
     run(updater)
-    
+
 ########################
 
 
