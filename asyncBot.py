dd25a7185741abbd0e3090e4205b4056ac78128a
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2021-06-16 22:48:16 +0800

    Update asyncBot.py

--- asyncBot.py
+++ asyncBot.py
@@ -6,6 +6,21 @@ CHAT_ID = 'https://t.me/hello_min'
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://ou7is.herokuapp.com/' + TOKEN)
+
+
+
+
 def send_message(event, context):
     bot = telegram.Bot(token=TOKEN)
     bot.sendMessage(chat_id = CHAT_ID, text = 'Daily reminder has been set! You\'ll get notified at 11 AM daily')
@@ -16,5 +31,8 @@ sched = BackgroundScheduler()
 sched.add_job(send_message, 'cron', day_of_week='mon-fri', hour=11, minute=30, end_date='2021-07-30')
 
 sched.start()
+
+if __name__ == '__main__':
+    main()
 
 
