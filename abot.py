19e7ae3c3584d4e2af38e026346fbe4094f0d26e
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2021-06-17 15:36:44 +0800

    Update bot.py

--- abot.py
+++ abot.py
@@ -1,10 +1,12 @@
-"""
-Deployed using heroku.
-"""
-from apscheduler.schedulers.blocking import BlockingScheduler
+
+import telebot
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
@@ -17,36 +19,35 @@ TOKEN = '1119831121:AAHb_nvYn1M5NciLJu1NX-48jMTkrPUZ0sc'
 CHAT_ID = '474164495'
 HEROKU_APP_NAME = 'https://ou7is.herokuapp.com/'
 
+# bot = telebot.TeleBot(TOKEN)
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
+
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Hi! I am a bot')
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-#######################################
 def send(update, context):
     update.send_message('@hello_min', "Daily reminder has been set! You\'ll get notified at 11 AM daily")
     update.message.reply_text("Hello")
     
+    # schedule.every().day.at("12:30").do(send)
 sched = BlockingScheduler()
 
 # Runs from Monday to Friday at 11:30 (am) until 2014-07-30 00:00:00
 sched.add_job(send, 'cron', day_of_week='mon-fri', hour=15, minute=7, end_date='2021-07-30')
 
-sched.start()    
-########################################
+sched.start()
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+################################
 
 def main():
     """Start the bot."""
@@ -60,11 +61,10 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("send", send))
-    
+
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
