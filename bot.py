90fa9b2b1f8673b91f74364b751d16c7c66a1193
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2021-06-17 15:16:57 +0800

    add dispatchers

--- bot.py
+++ bot.py
@@ -25,12 +25,11 @@ bot = telebot.TeleBot(TOKEN)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-@bot.message_handler(commands=["start"])
+
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi! I am a bot')
 
-@bot.message_handler(commands=["send"])
 def send(message):
     bot.send_message(message.chat.id,f"Daily reminder has been set! You\'ll get notified at 11 AM daily")
     bot.send_message(message('@hello_min'),f"Hello")
@@ -46,7 +45,6 @@ sched.start()
     #     schedule.run_pending()
     #     time.sleep(1)
 
-@bot.message_handler(commands=["error_log"])
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -62,6 +60,16 @@ def main():
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("send", send))
+
+    # on noncommand i.e message - echo the message on Telegram
+    # dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
