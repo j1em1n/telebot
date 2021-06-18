415bba0ad1809f46ce2d09bf59bdd89165f8ee30
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2021-06-18 16:42:33 +0800

    add handler

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ Deployed using heroku.
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
@@ -25,6 +26,23 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
+################################################
+def send(update, context):
+    update.send_message('@hello_min', "Daily reminder has been set! You\'ll get notified at 11 AM daily")
+    update.message.reply_text("Reminder sent!")
+    
+    # schedule.every().day.at("12:30").do(send)
+sched = BlockingScheduler()
+
+# Runs from Monday to Friday at 11:30 (am) until 2014-07-30 00:00:00
+sched.add_job(send, 'cron', day_of_week='mon-fri', hour=11, minute=30, end_date='2021-07-30')
+
+sched.start()
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
+
+################################################
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
@@ -50,6 +68,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("send", send))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
