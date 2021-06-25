df2732e57320781989bcbb40cccf6c16ffbf1eee
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2021-06-25 12:57:34 +0800

    change time

--- bot.py
+++ bot.py
@@ -41,10 +41,10 @@ def telegram_bot_sendtext(bot_message):
 # time.strftime('%X %x %Z')
 os.environ['TZ'] = 'Asia/Singapore'
 time.tzset()
-hour_minute = time('12:20')
-hour_minute1 = time('14:20')
+hour_minute = '13:00'
+hour_minute1 ='14:20'
 
-test = telegram_bot_sendtext("hola!")
+test = telegram_bot_sendtext("i am a bot!")
 # print(test)
 # schedule.every(10).seconds.do(test)
 schedule.every().day.at(hour_minute).do(test)
